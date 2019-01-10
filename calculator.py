132fbbc0b069a2588f0c6d0e61bbfe0fafff0e17
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-09 17:58:18 -0800

    Working calculator 2

--- calculator.py
+++ calculator.py
@@ -6,35 +6,40 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
-while True: 
-    user_input = input("")
-    words = user_input.split(" ")
-    num1 = int(words[1])
-   
-    num2 = int(words[2])
-    #while True: 
 
-    if words[0] == 'q':
+while True: 
+    user_input = input(" ")
+    words = user_input.split(" ")
+    token = words[0]
+
+    if len(words) == 2:
+        num1 = int(words[1])
+
+    elif len(words) == 3:
+        num1 = int(words[1])
+        num2 = int(words[2])    
+
+    if token  == 'q':
         break
     else:
-        if words[0] == "+":
-           print(add(num1, num2))
-        elif words[0] == "-":
+        if token  == "+":
+           print(add(num1, num2)
+        elif token == "-":
            print(subtract(num1, num2))
-        elif words[0] == "*":
+        elif token == "*":
            print(multiply(num1, num2))
-        elif words[0] == "/":
+        elif token == "/":
             print(divide(num1, num2))
-        elif words[0] == "square":
-            square(num1)
-        elif words[0] == "cube":
-            cube(num1)
-        elif words[0] == "power":
-            power(num1, num2)
-        elif words[0] == "mod":
-            mod(num1, num2)
+        elif token == "square":
+            print(square(num1))
+        elif token == "cube":
+            print(cube(num1))
+        elif token == "power":
+            print(power(num1, num2))
+        elif token == "mod":
+            print(mod(num1, num2))
         else:
             print("I'm sorry I didn't understand that. Please try again.")    
-   
+
    
 # Your code goes here
